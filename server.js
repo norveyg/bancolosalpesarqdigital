762e48dd09327c217a4720cf6ac09a4dd8375b10
Norvey <norveyg@gmail.com>
2018-11-12 23:12:03 -0500

    111

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var express  = require('express');
-var app      = express();                               
-var morgan = require('morgan');            
+var app      = express();                                      
 var bodyParser = require('body-parser');    
-var cors = require('cors');
- 
-app.use(morgan('dev'));                                        
+        
 app.use(bodyParser.urlencoded({'extended':'true'}));            
 app.use(bodyParser.json());                                     
-app.use(cors());
  
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
