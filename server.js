c8ab361415e2132abe844b4a947f375811be9933
Norvey <norveyg@gmail.com>
2018-11-14 00:59:49 -0500

    prueba

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 var express  = require('express');
 var app      = express();                                      
-var bodyParser = require('body-parser');    
-        
-app.use(bodyParser.urlencoded({'extended':'true'}));            
-app.use(bodyParser.json());                                     
- 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
- 
+
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
